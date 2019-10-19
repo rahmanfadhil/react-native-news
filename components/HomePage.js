e2bd503e3a316336e4a251c948aa8a52dea0213a
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-10-19 15:54:52 +0700

    Drawer

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, FlatList, TouchableOpacity } from "react-native";
+import { Text, View, FlatList, TouchableOpacity, Button } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createDrawerNavigator } from "react-navigation-drawer";
 
-export default function HomePage(props) {
+function HomePage(props) {
   const [news, setNews] = useState([]);
 
   useEffect(() => {
@@ -13,18 +15,34 @@ export default function HomePage(props) {
   }, []);
 
   return (
-    <FlatList
-      renderItem={({ item }) => (
-        <TouchableOpacity
-          onPress={() => props.navigation.navigate("Detail", item)}
-        >
-          <View style={{ padding: 10 }}>
-            <Text style={{ fontSize: 24 }}>{item.title}</Text>
-            <Text style={{ marginTop: 15 }}>{item.description}</Text>
-          </View>
-        </TouchableOpacity>
-      )}
-      data={news}
-    />
+    <View>
+      <FlatList
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => props.navigation.navigate("Detail", item)}
+          >
+            <View style={{ padding: 10 }}>
+              <Text style={{ fontSize: 24 }}>{item.title}</Text>
+              <Text style={{ marginTop: 15 }}>{item.description}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+        data={news}
+      />
+    </View>
   );
 }
+
+function AboutPage() {
+  return (
+    <View>
+      <Text>About Page</Text>
+    </View>
+  );
+}
+
+const DrawerNavigator = createDrawerNavigator({
+  Home: { screen: HomePage },
+  About: { screen: AboutPage }
+});
+export default createAppContainer(DrawerNavigator);
